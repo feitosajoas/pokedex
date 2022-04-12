5c4d56ead633eae95ee3595572f1c020af696ed1
feitosajoas <joasfeitosadelimasilva@gmail.com>
2022-04-12 17:37:10 -0300

    Melhorias no componente de header

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, MatGridListModule],
   exports: [HeaderComponent]
 })
 export class CoreModule {}
