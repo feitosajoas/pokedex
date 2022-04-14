d08857c528c847f710706f8f7abba0e983848640
feitosajoas <joasfeitosadelimasilva@gmail.com>
2022-04-14 16:29:26 -0300

    Material icon

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 
 @NgModule({
   declarations: [PokemonCardComponent],
-  imports: [CommonModule, MatCardModule, MatBadgeModule],
+  imports: [CommonModule, MatCardModule, MatBadgeModule, MatIconModule, MatGridListModule],
   exports: [PokemonCardComponent]
 })
 export class SharedModule {}
