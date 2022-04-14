e7a6527067674454ead3c7577c0e8109261c7db5
feitosajoas <joasfeitosadelimasilva@gmail.com>
2022-04-14 16:29:00 -0300

    Material Icon

--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { SharedModule } from '../shared/shared.module';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 @NgModule({
-  imports: [CommonModule, MatInputModule, SharedModule],
-  declarations: [PokemonListComponent]
+  declarations: [PokemonListComponent],
+  imports: [CommonModule, MatInputModule, SharedModule, MatIconModule]
 })
 export class MainModule {}
