b1fdd718796eb283e43ab8beb7ad48efc3883da2
Joás Feitosa <joasfeitosadelimasilva@gmail.com>
2022-04-15 02:15:47 -0300

    Adcionar e remover pokémon favorito

--- frontend/src/app/shared/pokemon-card/pokemon-card.component.ts
+++ frontend/src/app/shared/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CookieHelper } from 'src/app/core/utils/cookie-helper';
 
 @Component({
@@ -6,23 +6,36 @@ import { CookieHelper } from 'src/app/core/utils/cookie-helper';
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.scss']
 })
-export class PokemonCardComponent {
+export class PokemonCardComponent implements OnInit {
   @Input() name: string = '';
   @Input() image: string = '';
   @Input() height: number = 0;
   @Input() types: any[] = [];
   @Input() health: any;
   @Input() attackPower: number = 0;
+  isFavorite: boolean = false;
   constructor() {}
+
+  ngOnInit(): void {
+    this.hasFavorite();
+  }
 
   getName() {
     return this.name.charAt(0).toUpperCase() + this.name.slice(1);
   }
 
   onFavorite(name: string) {
-    if (CookieHelper.getFavoritePokemons().includes(name)) {
-      return CookieHelper.getFavoritePokemons().replace(name, '');
-    }
+    this.isFavorite = true;
     return CookieHelper.setFavoritePokemon(name);
   }
+
+  unFavorite(name: string) {
+    this.isFavorite = false;
+    return CookieHelper.unFavoritePokemon(name);
+  }
+
+  hasFavorite() {
+    return CookieHelper.getFavoritePokemons(this.name) ? this.isFavorite = true : this.isFavorite = false;
+  }
 }
+
