40404bdcbf613bcfe9c45252c9660ff88aa94a1c
feitosajoas <joasfeitosadelimasilva@gmail.com>
2022-04-18 14:15:45 -0300

    Removendo funções para cookies não utilizada.. implementar na v2

--- frontend/src/app/core/utils/cookie-helper.ts
+++ frontend/src/app/core/utils/cookie-helper.ts
@@ -1,21 +1,4 @@
-import { Cookie } from '../models/cookie.model';
-
 export class CookieHelper {
-  static cookieName = 'cpc_sc';
-  static favPokemon = 'fav_c';
-
-  static setAllPermissions() {
-    localStorage.setItem(
-      this.cookieName,
-      JSON.stringify({
-        strictlyNecessary: true,
-        functional: true,
-        targetting: true,
-        performance: true
-      })
-    );
-  }
-
   static setFavoritePokemon(id: string) {
     localStorage.setItem(id, id);
   }
@@ -26,26 +9,5 @@ export class CookieHelper {
 
   static getFavoritePokemons(id: string): string {
     return localStorage.getItem(id) ?? '';
-  }
-
-  static setPermissions(cookie: Cookie) {
-    localStorage.setItem(this.cookieName, JSON.stringify(cookie));
-  }
-
-  static getCookie() {
-    const cookieString = localStorage.getItem(this.cookieName);
-    if (cookieString) {
-      return JSON.parse(cookieString);
-    }
-    return {
-      strictlyNecessary: true,
-      functional: false,
-      targetting: false,
-      performance: false
-    };
-  }
-
-  static checkCookie() {
-    return localStorage.getItem(this.cookieName) ? false : true;
   }
 }
